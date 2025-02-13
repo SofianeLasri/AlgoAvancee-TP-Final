7b4a2e11185f33100bb94137f6fb56f6fe0281d3
SofianeLasri <alasri250@gmail.com>
2025-02-13 16:19:58 +0100

    Add analyze endpoint to process JSON tweets and return scores

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+# Je sais pas comment créer des commentaires similaires aux PhpDoc en Python mais on va faire comme ça :)
+# Nous n'acceptons que des requêtes POST donnant un objet JSON ayant cette structure :
+# { "tweets": [ "tweet content", "tweet content", "tweet content", ... ] }
+# La réponse sera un objet JSON ayant cette structure (score entre -1 et 1) :
+# { "tweet1": score1, "tweet2": score2, "tweet3": score3, ... }
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+    tweets = data['tweets']
+
+    result = {}
+    for i in range(len(tweets)):
+        tweetIndex = "tweet" + str(i)
+        result[tweetIndex] = 0.5
+
+    return result
 
 
 if __name__ == '__main__':
