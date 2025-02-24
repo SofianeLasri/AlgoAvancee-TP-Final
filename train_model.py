47152ca84044ac2f49d777d039f43ecdf8807462
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2025-02-24 21:01:55 +0100

    Update environment configuration and enhance model training with evaluation metrics

--- train_model.py
+++ train_model.py
@@ -3,6 +3,8 @@ import re
 from joblib import dump
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
 
 from database import db_session
 from models import Tweet
@@ -24,13 +26,25 @@ def train_model():
 
     vectorizer = CountVectorizer(stop_words=french_stopwords, max_features=100)
     X = vectorizer.fit_transform(texts)
+    Y = labels
+
+    # Division des données
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
     model = LogisticRegression()
-    model.fit(X, labels)
+    model.fit(X_train, y_train)
 
     dump(model, 'model.joblib')
     dump(vectorizer, 'vectorizer.joblib')
     print("Model trained and saved to disk.")
+
+    y_pred = model.predict(X_test)
+    # Rapport de classification
+    print("Rapport de classification :")
+    print(classification_report(y_test, y_pred))
+    # Matrice de confusion
+    print("Matrice de confusion :")
+    print(confusion_matrix(y_test, y_pred))
 
 def clean_text(text):
     text = text.lower()
